445e381d2e7cad9f66f66222b136d3079fd29177
Roger Nordqvist <progdev.squirrel@gmail.com>
2018-04-02 22:21:47 +0200

    refactored data.service to user.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { DataService } from './services/data.service';
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [DataService],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
